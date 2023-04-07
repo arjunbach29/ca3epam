c7ffcec24ed425e00cd5a05b73ed4bebc3129ca5
arjun <arjunbach@gmail.com>
2023-04-07 11:14:56 +0530

    pushing

--- src/test/java/org/example/mailtesting.java
+++ src/test/java/org/example/mailtesting.java
@@ -8,8 +8,20 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.*;
 import org.testng.Reporter;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class mailtesting {
     WebDriver tester;
+    private Properties prop;
+
+    @BeforeTest
+    public void beforeTest() throws IOException {
+        prop = new Properties();
+        FileInputStream fis = new FileInputStream("resources/config.properties");
+        prop.load(fis);
+    }
 
 
 
@@ -30,7 +42,7 @@ public class mailtesting {
     }
     @Test
     public void accessmail() throws InterruptedException {
-        tester.findElement(By.xpath("//input[@id='identifierId']")).sendKeys("arjunbacharwar29@gmail.com");
+        tester.findElement(By.xpath("//input[@id='identifierId']")).sendKeys(prop.getProperty("username"));
         Thread.sleep(1000);
         tester.findElement(By.xpath("//span[normalize-space()='Next']")).click();
         Thread.sleep(10000);
